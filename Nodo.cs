1a6010276fa592bc3cfba60b46c5a962521d6f65
Juaan26 <Juaan26g@gmail.com>
2026-07-08 14:15:56 +0200

    cambiado como se establece que Nodo sea Icomparable

--- Nodo.cs
+++ Nodo.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ListaEnlazada
 {
-    class Nodo<T>:IComparable<Nodo<T>> 
+    class Nodo<T>:IComparable<Nodo<T>> where T : IComparable<T>
     {
         public T Valor { get; set; }
         public Nodo<T>? Siguiente {  get; set; }
@@ -16,9 +16,7 @@ namespace ListaEnlazada
 
         public int CompareTo(Nodo<T>? other)
         {
-            if (Comparer<T>.Default.Compare(Valor, other.Valor) >= 0) return 1;
-            if (Comparer<T>.Default.Compare(Valor, other.Valor) < 0) return -1;
-            return 0;
+            return Valor.CompareTo(other.Valor);
         }
     }
 }
